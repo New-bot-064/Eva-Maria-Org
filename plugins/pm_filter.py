0f83adc2fcfb1ecadca6d9d7f1d751dc8765f150
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-04-04 17:16:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -414,11 +414,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/ArkBotz')
+            InlineKeyboardButton('🕵 Go Inline', switch_inline_query=''),
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
         ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/ArkBotz'),
+            InlineKeyboardButton('🔐 Close', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
