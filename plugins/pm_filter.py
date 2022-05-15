a9109f8cd32bacd128b7cb550f972154b3b9d945
Magic-Art-Room <99250685+Magic-Art-Room@users.noreply.github.com>
2022-05-15 17:56:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -709,7 +709,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     else:
         cap = f"© Bot Managed By @ArkBotz ★Here is what i found for your query {search}"
-       if imdb and imdb.get('poster'):
+      if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
             await asyncio.sleep(600)
